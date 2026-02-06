eddac4ed36eeea0970bb7496c45ceda9e3704942
knooph <grantnw517@gmail.com>
2026-02-06 16:53:56 -0500

    template
    Created this template so i can just copy and build off of this in future

---
+++ spring_2026/template/network.cpp
@@ -0,0 +1,4 @@
+#include "network_id.h"
+
+const char* WiFi_SSID = "puckmelt";
+const char* WiFi_PASSWORD = "3vilR0omba";
---
+++ spring_2026/template/template.ino
@@ -0,0 +1,12 @@
+#include "OTA.h"
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_RGB_LED, OUTPUT);
+  OTA_init();
+}
+
+void loop() {
+  OTA_handle();
+
+}
