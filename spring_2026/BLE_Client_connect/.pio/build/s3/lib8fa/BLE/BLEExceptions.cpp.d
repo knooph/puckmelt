e0a45f0f56640eebae79452cb381ed7edfbf82ab
knooph <grantnw517@gmail.com>
2026-02-26 11:30:12 -0500

    Delete spring_2026/BLE_Client_connect/.pio directory
    platformio arifact

--- spring_2026/BLE_Client_connect/.pio/build/s3/lib8fa/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/s3/lib8fa/BLE/BLEExceptions.cpp.o: \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
--- spring_2026/BLE_Client_connect/.pio/build/s3/lib8fa/BLE/BLESecurity.cpp.d
+++
@@ -1,8 +0,0 @@
-.pio/build/s3/lib8fa/BLE/BLESecurity.cpp.o: \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.cpp \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLESecurity.h \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/qio_qspi/include/sdkconfig.h \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/bt/host/bluedroid/api/include/api/esp_gap_ble_api.h \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_err.h \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/esp_common/include/esp_compiler.h \
- C:/Users/willig6/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/bt/host/bluedroid/api/include/api/esp_bt_defs.h
