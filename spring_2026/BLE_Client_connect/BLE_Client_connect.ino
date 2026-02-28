f0b04a9b2af707b41b0b91c7a6f57185c6171c09
knooph <grantnw517@gmail.com>
2026-02-28 02:11:32 -0500

    Further development

--- spring_2026/BLE_Client_connect/BLE_Client_connect.ino
+++ spring_2026/BLE_Client_connect/BLE_Client_connect.ino
@@ -2,13 +2,64 @@
 #include "bluetooth.h"
 #include <Arduino.h>
 
-bluetooth bt;
+bluetooth* bt; //this is a pointer because BLE needs arduino runtime drivers to be set up
+void handle_Serial();
+void cmd_Serial(std::string cmd);
+std::string input_buffer;
+bool holdSerial = false;
 
 void setup() {
   Serial.begin(115200);
+  Serial.setTimeout(30000);
   OTA_init();
+  bt = new bluetooth();
 }
 
 void loop() {
   OTA_handle();
+  handle_Serial();
+}
+
+void handle_Serial() {
+  if (Serial.available() > 0 && !holdSerial) { //Generic Serial input
+    char c = Serial.read();
+    input_buffer += c;
+    Serial.print(c);
+    if (c == '\n') {
+      Serial.println("cmd detected");
+      input_buffer.pop_back();
+      Serial.println(input_buffer.c_str());
+      cmd_Serial(input_buffer);
+      input_buffer = "";
+    } else if (c == '\b') {
+      input_buffer.pop_back();
+      Serial.print(" ");
+      input_buffer.pop_back();
+    }
+  } else if (holdSerial && Serial.peek() == 1 && !bt->isScanning) { //Hold serial while scanning
+      for (int i = 0; i < bt->results->getCount(); i++) {
+        Serial.print(i);
+        Serial.print(") ");
+        Serial.print(bt->results->getDevice(i).toString().c_str());
+      Serial.flush();
+      holdSerial = false;
+    }
+  }
+}
+
+void cmd_Serial(std::string cmd) {
+  // Serial.println(cmd.substr(0,5).c_str());
+  // Serial.println(cmd.substr(5).c_str());
+  // for (int i = 0; i < String(cmd.substr(5).c_str()).toInt(); i++) {
+  //   Serial.println(cmd.substr(0,5).c_str());
+  // }
+  if (cmd.substr(0,5).compare("scan") && !cmd.substr(5).empty()) {
+    holdSerial = true;
+    //If cmd is "scan number", convert number to c_str to build arduino String to convert to Integer
+    Serial.print("Scanning for ");
+    Serial.print(String(cmd.substr(5).c_str() ).toInt());
+    Serial.println(" seconds.");
+    bt->scan( String( cmd.substr(5).c_str() ).toInt() );
+    Serial.println( (bt->isScanning) ? "true" : "false");
+  }
 }
--- spring_2026/BLE_Client_connect/bluetooth.h
+++ spring_2026/BLE_Client_connect/bluetooth.h
@@ -5,18 +5,43 @@
 #include <BLESecurity.h>
 #include <string.h>
 
+//Forward Declarations
+class GappCallback; //line 40
+
+struct init_wrapper {
+    init_wrapper();
+};
+
 class bluetooth {
     private:
-        BLEScan* scanner = nullptr;
-        BLEAdvertisedDevice target;
-        BLEAddress target_addr;
-        BLEScanResults* results = nullptr;
-        std::string output_buffer;
+        init_wrapper initializer;
+        static BLEScan* scanner; //Scanner object pointer
+        static BLEAdvertisedDevice* target; //Pointer to target BLE peripheral because it can't be 
+        static BLEAddress* target_addr;     //initialized in link time
+        BLEAdvertisedDevice target_mem; //actual non-static member that will be pointed to
+        BLEAddress target_addr_mem; //actual non-static member that will be pointed to
+        BLEScanResults results_mem; //actual non-static member that will be pointed to
+        static BLEClient* client; //The client being used to connect to a peripheral (in heap)
+        
     public:
-        static bool isScanning;
+        static bool isScanning; //if the device is scanning for peripherals
+        static BLEScanResults* results; //point to the internal scan result storage
+
         bluetooth();
-        void init();
+        ~bluetooth();
         void scan(uint32_t seconds);
-        void setTarget(std::string mac_addr);
-        bool setTarget(uint32_t device_index);
+        void setTarget(std::string mac_addr); //Set target device to connect to based off of addres
+        bool setTarget(uint32_t device_index); //set taret device from scan results
+    
+    //allow Callbacks to access private members
+    friend class GappCallback;
 };
+
+class GappCallback : public BLEAdvertisedDeviceCallbacks {
+    public:
+        static int index;
+        static std::string output_buffer;
+        void onResult(BLEAdvertisedDevice advertisedDevice);
+        static void scanCallback(BLEScanResults results);
+};
+
