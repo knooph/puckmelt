9b89534a80a714193355ec0c25b3a5a1bf9876b2
knooph <grantnw517@gmail.com>
2026-03-31 16:52:24 -0400

    Update network_id.h

--- spring_2026/crsf_test/network_id.h
+++ spring_2026/crsf_test/network_id.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define TH_CHANNEL 0
-#define FB_CHANNEL 1
-#define LR_CHANNEL 2
+#define TH_CHANNEL 1
+#define FB_CHANNEL 2
+#define LR_CHANNEL 3
 
 extern const char* WiFi_SSID; //define in network_id.cpp
 extern const char* WiFi_PASSWORD;
