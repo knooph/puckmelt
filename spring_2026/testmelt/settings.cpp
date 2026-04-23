75dc47f5faf6c5371d509bf4631853774237764c
arnavbhatnagar50 <arnavbhatnagar50@gmail.com>
2026-04-23 18:05:02 -0400

    Change MAX_RPM_MTR from 30000 to 13320

--- spring_2026/testmelt/settings.cpp
+++ spring_2026/testmelt/settings.cpp
@@ -6,5 +6,5 @@ const char* WiFi_PASSWORD = "3vilR0omba";
 const int SERVER_PORT = 32;
 
 double TIME_INTVL = 1/2400000;
-uint MAX_RPM_MTR = 30000;
+uint MAX_RPM_MTR = 13320;
 uint8_t FLIP_SENSITIVITY = 5;
